e2cb2d802569165d2bb6c23fc4532f4b3f7b00b8
Maxim Tsyganov <0xc00000f@gmail.com>
2022-04-14 02:05:02 +0300

    Initial Commit

---
+++ game-of-chats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  game-of-chats
+//
+//  Created by Maxim Tsyganov on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
